d0dd27386def2901fd69aeb5bc1a094d26fa6ee5
Tuturado <alef23073@gmail.com>
2021-05-07 06:43:21 -0300

    Primeiro commit

---
+++ gerador.js
@@ -0,0 +1,21 @@
+let bottom = document.getElementById("gerar");
+let aleatorio;
+let resultado = [];   
+bottom.addEventListener("click", gerar_senha);
+   
+
+function gerar_senha(){
+   for(let i=0; i<9;i++){
+      maiusculeChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+      minusculeChars = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+      chars = ['@', '!', '#', '$', '%', '&', '*','-'];
+      maiusculeAletory = maiusculeChars[Math.floor(Math.random()* maiusculeChars.length)];
+      minusculeAleatory = minusculeChars[Math.floor(Math.random()* minusculeChars.length)];
+      charsAleatory = chars[Math.floor(Math.random() *chars.length)];
+      aleatorio = Math.random() * 9;
+      resultado[i] = Math.floor(aleatorio);   
+   }
+   
+   document.getElementById("senha").innerHTML = resultado.concat(maiusculeAletory).concat(minusculeAleatory).concat(charsAleatory).join('');
+   
+}
