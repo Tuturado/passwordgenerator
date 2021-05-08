e81d2e31ddcfecb2d0d7aea04db38156ee68b0c2
Tuturado <alef23073@gmail.com>
2021-05-07 21:16:43 -0300

    Criado em formato de extensão pronta para rodar no Google Chrome

--- gerador.js
+++ gerador.js
@@ -5,7 +5,7 @@ bottom.addEventListener("click", gerar_senha);
    
 
 function gerar_senha(){
-   for(let i=0; i<9;i++){
+   for(let i=0; i<8;i++){
       maiusculeChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
       minusculeChars = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
       chars = ['@', '!', '#', '$', '%', '&', '*','-'];
