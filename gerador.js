aca8a952fe9069ebdefcda530332f9067c1df674
Tuturado <alef23073@gmail.com>
2021-10-07 20:29:38 -0300

    Refatoração da função gerarSenha para formato Arrow Function.

--- gerador.js
+++ gerador.js
@@ -1,21 +1,32 @@
 let bottom = document.getElementById("gerar");
-let aleatorio;
-let resultado = [];   
-bottom.addEventListener("click", gerar_senha);
-   
+bottom.addEventListener("click", function(e){
+   e.preventDefault();
+   gerarSenha();
+});
 
-function gerar_senha(){
-   for(let i=0; i<8;i++){
+
+let gerarSenha = () => {
+   let aleatorio;
+   let aux = []
+   let resultado;
+   let maiusculeChars;
+   let minusculeChars;
+   let chars;
+   let maiusculeAletory;
+   let minusculeAleatory;
+   let charsAleatory;
+   for (let i = 0; i < 8; i++) {
       maiusculeChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-      minusculeChars = ['a', 'b' , 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-      chars = ['@', '!', '#', '$', '%', '&', '*','-'];
-      maiusculeAletory = maiusculeChars[Math.floor(Math.random()* maiusculeChars.length)];
-      minusculeAleatory = minusculeChars[Math.floor(Math.random()* minusculeChars.length)];
-      charsAleatory = chars[Math.floor(Math.random() *chars.length)];
+      minusculeChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+      chars = ['@', '!', '#', '$', '%', '&', '*', '-'];
+      maiusculeAletory = maiusculeChars[Math.floor(Math.random() * maiusculeChars.length)];
+      minusculeAleatory = minusculeChars[Math.floor(Math.random() * minusculeChars.length)];
+      charsAleatory = chars[Math.floor(Math.random() * chars.length)];
       aleatorio = Math.random() * 9;
-      resultado[i] = Math.floor(aleatorio);   
+      aux[i] = Math.floor(aleatorio);
    }
-   
-   document.getElementById("senha").innerHTML = resultado.concat(maiusculeAletory).concat(minusculeAleatory).concat(charsAleatory).join('');
-   
+
+   return resultado=document.getElementById("senha").innerHTML = aux.concat(maiusculeAletory).concat(minusculeAleatory).concat(charsAleatory).join('');
+
 }
+
